010db1b38dc3648c33b05b56d55917236a5bd3ac
KEXIN DENG <Traky@Bennet-the-Rabbit.local>
2020-02-28 11:34:21 -0500

    feb 28 on the bus

--- Lake_Erie.R
+++ Lake_Erie.R
@@ -22,7 +22,24 @@ plot(LE_ts,
      xlab = "",
      ylab = "Lake Erie Water Levels in Tens of Meters")
 
-# resources
+plot(diff(LE_ts),
+     main = "Diff - Monthly Lake Erie Levels (1921 – 1970)",
+     xlab = "",
+     ylab = "Lake Erie Water Levels in Tens of Meters")
+
+# log transformation
+plot(diff(log(LE_ts)))
+
+# power transformation
+BoxCox.ar(LE_ts,lambda = seq(1.45,1.6,0.01))
+
+lambda = 1.55
+
+plot((LE_ts)^(lambda))
+plot(diff((LE_ts)^(lambda)))
+
+## trying estimating parameters with both transformations
+
 # https://kimberlyannschveder.wordpress.com/2018/07/07/lake-erie-level-data/
 # https://towardsdatascience.com/a-time-series-analysis-of-lake-erie-from-1921-to-1970-using-a-sarima-model-b79698df4762
 
