1d3c5853ca189d9a224b06fa8284c9c0339cd860
KEXIN DENG <Traky@Bennet-the-Rabbit.local>
2020-02-28 00:17:45 -0500

    initial

---
+++ Lake_Erie.R
@@ -0,0 +1,28 @@
+
+LE <- read.csv("/Users/Traky/Desktop/Group Project/Lake_Erie.csv")
+
+# take out first column
+LE <- LE[, -1]
+
+# reorder column
+LE <- LE[, c(2, 1)]
+
+# Convert to a ts object
+LE_ts <- ts(
+  LE,
+  start = c(1921, 1),
+  end = c(1970, 12),
+  frequency = 12
+)
+LE_ts <- LE_ts[, -1]
+
+# plotting
+plot(LE_ts,
+     main = "Monthly Lake Erie Levels (1921 – 1970)",
+     xlab = "",
+     ylab = "Lake Erie Water Levels in Tens of Meters")
+
+# https://kimberlyannschveder.wordpress.com/2018/07/07/lake-erie-level-data/
+# https://towardsdatascience.com/a-time-series-analysis-of-lake-erie-from-1921-to-1970-using-a-sarima-model-b79698df4762
+
+
