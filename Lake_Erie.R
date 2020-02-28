9082f1c0eb5669b136ba5aea0a31867b8ca5702b
Traky <dengk7@mcmaster.ca>
2020-02-28 14:19:13 -0500

    new

--- Lake_Erie.R
+++ Lake_Erie.R
@@ -1,4 +1,6 @@
 
+
+
 LE <- read.csv("/Users/Traky/Desktop/4A03_Project/Lake_Erie.csv")
 
 # take out first column
@@ -31,16 +33,37 @@ plot(diff(LE_ts),
 plot(diff(log(LE_ts)))
 
 # power transformation
-BoxCox.ar(LE_ts,lambda = seq(1.45,1.6,0.01))
+BoxCox.ar(LE_ts, lambda = seq(1.45, 1.6, 0.01))
 
 lambda = 1.55
 
-plot((LE_ts)^(lambda))
-plot(diff((LE_ts)^(lambda)))
+plot((LE_ts) ^ (lambda))
+plot(diff((LE_ts) ^ (lambda)))
 
 ## trying estimating parameters with both transformations
 
 # https://kimberlyannschveder.wordpress.com/2018/07/07/lake-erie-level-data/
 # https://towardsdatascience.com/a-time-series-analysis-of-lake-erie-from-1921-to-1970-using-a-sarima-model-b79698df4762
+
+
+
+# Note that the order of moving average corresponds to seasonal frequency, which is
+# usually a first step in classical time series decomposition.
+
+# step 1 - find seasonality
+library(TSA)
+
+# Fourier Transformation
+p <- periodogram(LE_ts)
+p$freq[which.max(p$spec)] # 1/12 USELESS LOL
+
+p_df <- data.frame(freq=p$freq, spec=p$spec)
+
+# step 2 - find entered MA by taking out the seasonality 
+trend_beer = ma(timeserie_beer, order = 4, centre = T)
+plot(as.ts(timeserie_beer))
+lines(trend_beer)
+plot(as.ts(trend_beer))
+
 
 
